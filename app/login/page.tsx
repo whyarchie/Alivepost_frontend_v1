655b27816fd19bd1283484efe0f455d320e8a979
whyarchie <http.shivamsingh@gmail.com>
2026-04-06 22:48:14 +0530

    Login page api integration

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,9 +1,12 @@
+import { Suspense } from "react";
 import { LoginForm } from "@/components/login-form";
 
 export default function LoginPage() {
   return (
     <div className="min-h-screen w-full">
-      <LoginForm className="h-screen w-full" />
+      <Suspense fallback={<div className="min-h-screen w-full" />}>
+        <LoginForm className="h-screen w-full" />
+      </Suspense>
     </div>
   )
 }
